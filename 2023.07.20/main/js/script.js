6113493ee25b293441b4fc392b9c045c45fbed65
BJH <nowaffle1753@gmail.com>
2023-07-28 18:09:33 +0900

    2023.07.28

--- 2023.07.20/main/js/script.js
+++ 2023.07.20/main/js/script.js
@@ -35,14 +35,23 @@ window.onload = function () {
       NEW_GOOD = obj.newgood;
       RECOMMEND = obj.recommendgood;
       POPULAR_ICON = obj.popularicon;
+      POPULAR_GOOD = obj.populargood;
+      BRAND_ARR = obj.brandarr;
+
       // 비주얼 화면에 배치한다
       showVisual();
+      // 오늘의 상품을 화면에 배치한다.
       showTodayGood();
+      // 할인상품을 화면에 배치한다.
       showSaleGood();
       showNewGood();
       showRecommendGood();
-      // 인기물품 아이콘 화면 배친
+      // 인기물품 아이콘 화면 배ㅊ;
       showPoplarIconGood();
+      // 인기 물품 화면 배치
+      showPoplarGood();
+      // 브랜드 목록을 화면에 배치
+      showBrandArr();
     }
   };
   // 자료를 호출한다.
@@ -72,6 +81,15 @@ window.onload = function () {
   // 인기물품 아이콘 화면 출력 기능
   let POPULAR_ICON;
   let popularIconTag = document.getElementById("data-popular-icon");
+
+  // 인기 물품 화면에 출력 기능
+  let POPULAR_GOOD;
+  let popularshow = 1; //목록 중에 0번을 보여준다.
+  let popularTag = document.getElementById("data-popular");
+
+  // 브랜드 목록 화면에 출력 기능
+  let BRAND_ARR;
+  let brandTag = document.getElementById("data-brand");
 
   // ======================================
 
@@ -353,6 +371,113 @@ window.onload = function () {
       navigation: {
         nextEl: ".popular-slide-next",
         prevEl: ".popular-slide-prev",
+      },
+    });
+    // .popular-cate-icon에 호버했을때 이미지 변경 코드
+    const tag = document.querySelectorAll(".popular-slide a");
+    tag.forEach(function (item, index) {
+      // 호버 했을때 이미지가 변경
+      item.addEventListener("mouseover", function () {
+        const spanTag = this.querySelector(".popular-cate-icon");
+        spanTag.style.backgroundPositionY = "-64px";
+      });
+      // 마우스가 아웃됬을때
+      item.addEventListener("mouseout", function () {
+        const spanTag = this.querySelector(".popular-cate-icon");
+        spanTag.style.backgroundPositionY = "0";
+      });
+      //클릭하면 버튼(.popular--more)의 글자를
+      // 클릭된 타이틀의 글자로 변경
+      item.addEventListener("click", function (event) {
+        // a테그이므로 hrdf적용된다.
+        // 웹브라우저 갱신이 되므로 UI를 위해 막아야 한다.
+        event.preventDefault();
+        const bt = document.querySelector(".popular-more");
+        const title = this.querySelector(".popular-cate-name");
+        bt.innerHTML = `${title.innerHTML} 물품 더보기`;
+        // 하단의 목록을 갱신한다.
+        // 현재 클릭된 번호를 popularshow 에 담는다.
+        popularshow = index;
+        showPoplarGood();
+      });
+    });
+  }
+  // 이재는 인기 상품 화면 출력
+  function showPoplarGood() {
+    let html = "";
+    let popCate = "populargood-" + (popularshow + 1);
+    console.log(POPULAR_GOOD[popCate]);
+    POPULAR_GOOD[popCate].forEach(function (item) {
+      let tag = `
+      <div class="good-box">
+      <!-- 제품이미지 -->
+      <a href="${item.link}" class="good-img">
+        <img src="images/${item.pic}" alt="${item.name}" />
+        <span class="good-type">${item.tag}</span>
+      </a>
+      <!-- 제품정보 -->
+      <a href="${item.link}" class="good-info">
+        <em>${item.name}</em>(<em>${item.unit}</em>)
+      </a>
+      <!-- 제품가격 -->
+      <a href="${item.link}" class="good-info-price">${priceToString(
+        item.price
+      )}<em>원</em></a>
+      <!-- 장바구니 -->
+      <button class="good-add-cart"></button>
+    </div>
+      `;
+      html += tag;
+      popularTag.innerHTML = html;
+    });
+  }
+
+  // 브랜드 목록 화면 출력 기능
+  function showBrandArr() {
+    let html = `
+  <div class="swiper sw-brand">
+  <div class="swiper-wrapper">
+    `;
+    BRAND_ARR.forEach(function (item) {
+      let tag = `
+      <div class="swiper-slide">
+      <div class="brand-box">
+      <a href="${item.link}">
+        <img src="../images/${item.pic}" alt = " ${item.name}"/>
+          <p> ${item.name} </p>
+            <ul class="brand-info clearfix">
+              <li>
+                <span class="brand-info-title"> ${item.title1} </span>
+                <span class="brand-info-value"> ${item.value1} </span>
+              </li>
+              <li>
+                <span class="brand-info-title"> ${item.title2} </span>
+                <span class="brand-info-value"> ${item.value2} </span>
+              </li>
+            </ul>
+      
+      </a>
+
+      </div>
+      </div>
+      `;
+      html += tag;
+    });
+    html += `
+    </div>
+      </div>
+    `;
+    brandTag.innerHTML = html;
+    const swBrand = new Swiper(".sw-brand", {
+      slidesPerView: 3,
+      spaceBetween: 16,
+      navigation: {
+        prevEl: ".brand .slide-prev",
+        nextEl: ".brand .slide-next",
+      },
+      pagination: {
+        el: ".brand .slide-pg",
+        type: "fraction",
       },
     });
   }
